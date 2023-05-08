9616a4d857a6a9183d0da684034d4078385316ad
JoseGomes <jgsj@ic.ufal.br>
2023-05-07 22:12:54 -0300

    Update lands_parameters.py

--- lands_parameters.py
+++ lands_parameters.py
@@ -12,23 +12,32 @@ def landsParameters(land, level):
     if level == 1:
       damping = 0.4
     if level == 3:
-      damping = 0.1
-    if level == 4:
+      upperLimit = 0.7
+      lowerLimit = -0.7
       damping = 0.001
+    if level == 4:
+      damping = 0.25
   # Non-linearLand
   elif land == 2:
     damping = 0.65
+    if level == 3:
+          damping = 0.01
     if level == 4:
       damping = 0.35
   # SwitchingLand
   elif land == 3:
-    damping = 0.45
+    damping = 0.4
+    upperLimit = 0.40
+    lowerLimit = -0.40
     if level == 2:
-      upperLimit = 0.55
-      lowerLimit = -0.55
+      upperLimit = 0.40
+      lowerLimit = -0.40
     elif level == 3:
-      upperLimit = -0.34
+      upperLimit = 0.2
+      lowerLimit = -0.2
+      damping = 0.2
     elif level == 4:
+      upperLimit = 0.7
       lowerLimit = -0.20
   # QuantizedLand
   elif land == 4:
